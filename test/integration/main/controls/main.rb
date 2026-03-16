41f865ac02d6f5523bad28919d845621102d07aa
Macho Prawn <arun.oslinks@gmail.com>
2026-03-16 11:34:32 +0000

    TF Module / GCP / VPN Fabric / Publish 1.0.0

--- test/integration/main/controls/main.rb
+++ test/integration/main/controls/main.rb
@@ -1,36 +1,52 @@
 title "Main Resources"
 
-###################################################################################
-#
-# Write test cases here
-#
-###################################################################################
+src_project_id = input("src_project_id")
+dst_project_id = input("dst_project_id")
+src_region = input("src_region")
+dst_region = input("dst_region")
+src_router_name = input("src_router_name")
+dst_router_name = input("dst_router_name")
+src_gateway_name = input("src_gateway_name")
+dst_gateway_name = input("dst_gateway_name")
+src_tunnel_names = input("src_tunnel_names")
+dst_tunnel_names = input("dst_tunnel_names")
 
-# Below is the test case to check if a bucket is present in the correct location
+control "gcp-vpn-source-1.0" do
+  impact 1.0
+  title "Ensure source VPN resources exist"
 
-=begin
-# load the inputs (output by Terraform, stored in inputs.yaml by the pipeline)
-bucket_name = input("bucket_name")
-
-control "gcp-bucket-1.0" do                             # A unique ID for this control
-  impact 1.0                                            # The criticality, if this control fails.
-  title "Ensure bucket is correct"                      # A human-readable title
-
-  describe google_storage_bucket(name: bucket_name) do
+  describe google_compute_router(project: src_project_id, region: src_region, name: src_router_name) do
     it { should exist }
-    its('location') { should cmp 'US' }
-    its('storage_class') { should eq "STANDARD" }
-    
-    # Not supported by inspec-gcp :(
-    #its('uniform_bucket_level_access') { should eq true }
-    
-    # doesn't work because this is a Terraform meta directive, not a Google concept?
-    #its('force_destroy') { should eq true }
   end
 
-  describe google_storage_bucket(name: "nonexistent") do
-    it { should_not exist }
+  describe google_compute_vpn_gateways(project: src_project_id, region: src_region) do
+    it { should exist }
+    its("names") { should include src_gateway_name }
   end
 
+  src_tunnel_names.each do |tunnel_name|
+    describe google_compute_vpn_tunnel(project: src_project_id, region: src_region, name: tunnel_name) do
+      it { should exist }
+    end
+  end
 end
-=end
+
+control "gcp-vpn-destination-1.0" do
+  impact 1.0
+  title "Ensure destination VPN resources exist"
+
+  describe google_compute_router(project: dst_project_id, region: dst_region, name: dst_router_name) do
+    it { should exist }
+  end
+
+  describe google_compute_vpn_gateways(project: dst_project_id, region: dst_region) do
+    it { should exist }
+    its("names") { should include dst_gateway_name }
+  end
+
+  dst_tunnel_names.each do |tunnel_name|
+    describe google_compute_vpn_tunnel(project: dst_project_id, region: dst_region, name: tunnel_name) do
+      it { should exist }
+    end
+  end
+end
