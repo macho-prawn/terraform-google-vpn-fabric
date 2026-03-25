8fb7471ba129cb81ea9d0acd17bd0aa0aef18283
Macho Prawn <arun.oslinks@gmail.com>
2026-03-25 08:04:55 +0000

    TF Module / GCP / VPN Fabric / Publish 1.1.0

--- test/fixtures/main/outputs.tf
+++ test/fixtures/main/outputs.tf
@@ -6,19 +6,9 @@ locals {
     "cr-${gateway.peer1_gws.env_name}-${gateway.peer1_gws.gw_index[0]}-${gateway.peer1_gws.region_abbr}-${gateway.peer1_gws.gw_name}"
   ]))
 
-  dst_router_names = sort(distinct([
-    for gateway in local.gateways :
-    "cr-${gateway.peer2_gws.env_name}-${gateway.peer2_gws.gw_index[0]}-${gateway.peer2_gws.region_abbr}-${gateway.peer2_gws.gw_name}"
-  ]))
-
   src_gateway_names = sort(distinct([
     for gateway in local.gateways :
     "gw-${gateway.peer1_gws.env_name}-${gateway.peer1_gws.gw_index[0]}-${gateway.peer1_gws.region_abbr}-${gateway.peer1_gws.gw_name}"
-  ]))
-
-  dst_gateway_names = sort(distinct([
-    for gateway in local.gateways :
-    "gw-${gateway.peer2_gws.env_name}-${gateway.peer2_gws.gw_index[0]}-${gateway.peer2_gws.region_abbr}-${gateway.peer2_gws.gw_name}"
   ]))
 
   src_tunnel_names = sort([
@@ -26,48 +16,58 @@ locals {
     "tunnel-${tunnel.peer1_gws.env_name}-${tunnel.peer1_gws.gw_index[0]}-${tunnel.peer1_gws.region_abbr}-${tunnel.peer1_tunnels.tunnel_name}"
   ])
 
-  dst_tunnel_names = sort([
-    for tunnel in values(module.vpn_infra.tunnels) :
-    "tunnel-${tunnel.peer2_gws.env_name}-${tunnel.peer2_gws.gw_index[0]}-${tunnel.peer2_gws.region_abbr}-${tunnel.peer2_tunnels.tunnel_name}"
-  ])
+  dst_project_ids = sort(distinct([
+    for gateway in local.gateways : gateway.peer2_gws.project
+  ]))
+
+  dst_projects = {
+    for project_id in local.dst_project_ids : project_id => {
+      region = one(distinct([
+        for gateway in local.gateways : gateway.peer2_gws.region
+        if gateway.peer2_gws.project == project_id
+      ]))
+
+      router_names = sort(distinct([
+        for gateway in local.gateways :
+        "cr-${gateway.peer2_gws.env_name}-${gateway.peer2_gws.gw_index[0]}-${gateway.peer2_gws.region_abbr}-${gateway.peer2_gws.gw_name}"
+        if gateway.peer2_gws.project == project_id
+      ]))
+
+      gateway_names = sort(distinct([
+        for gateway in local.gateways :
+        "gw-${gateway.peer2_gws.env_name}-${gateway.peer2_gws.gw_index[0]}-${gateway.peer2_gws.region_abbr}-${gateway.peer2_gws.gw_name}"
+        if gateway.peer2_gws.project == project_id
+      ]))
+
+      tunnel_names = sort(distinct([
+        for tunnel in values(module.vpn_infra.tunnels) :
+        "tunnel-${tunnel.peer2_gws.env_name}-${tunnel.peer2_gws.gw_index[0]}-${tunnel.peer2_gws.region_abbr}-${tunnel.peer2_tunnels.tunnel_name}"
+        if tunnel.peer2_gws.project == project_id
+      ]))
+    }
+  }
 }
 
 output "src_project_id" {
   value = one(distinct([for gateway in local.gateways : gateway.peer1_gws.project]))
 }
 
-output "dst_project_id" {
-  value = one(distinct([for gateway in local.gateways : gateway.peer2_gws.project]))
-}
-
 output "src_region" {
   value = one(distinct([for gateway in local.gateways : gateway.peer1_gws.region]))
-}
-
-output "dst_region" {
-  value = one(distinct([for gateway in local.gateways : gateway.peer2_gws.region]))
 }
 
 output "src_router_names" {
   value = local.src_router_names
 }
 
-output "dst_router_names" {
-  value = local.dst_router_names
-}
-
 output "src_gateway_names" {
   value = local.src_gateway_names
-}
-
-output "dst_gateway_names" {
-  value = local.dst_gateway_names
 }
 
 output "src_tunnel_names" {
   value = local.src_tunnel_names
 }
 
-output "dst_tunnel_names" {
-  value = local.dst_tunnel_names
+output "dst_projects" {
+  value = local.dst_projects
 }
--- test/integration/main/controls/main.rb
+++ test/integration/main/controls/main.rb
@@ -1,15 +1,11 @@
 title "Main Resources"
 
 src_project_id = input("src_project_id")
-dst_project_id = input("dst_project_id")
+dst_projects = input("dst_projects")
 src_region = input("src_region")
-dst_region = input("dst_region")
 src_router_names = input("src_router_names")
-dst_router_names = input("dst_router_names")
 src_gateway_names = input("src_gateway_names")
-dst_gateway_names = input("dst_gateway_names")
 src_tunnel_names = input("src_tunnel_names")
-dst_tunnel_names = input("dst_tunnel_names")
 
 control "gcp-vpn-source-1.0" do
   impact 1.0
@@ -39,22 +35,29 @@ control "gcp-vpn-destination-1.0" do
   impact 1.0
   title "Ensure destination VPN resources exist"
 
-  dst_router_names.each do |router_name|
-    describe google_compute_router(project: dst_project_id, region: dst_region, name: router_name) do
-      it { should exist }
-    end
-  end
+  dst_projects.each do |dst_project_id, dst_project|
+    dst_region = dst_project.fetch("region")
+    dst_router_names = dst_project.fetch("router_names")
+    dst_gateway_names = dst_project.fetch("gateway_names")
+    dst_tunnel_names = dst_project.fetch("tunnel_names")
 
-  describe google_compute_vpn_gateways(project: dst_project_id, region: dst_region) do
-    it { should exist }
-    dst_gateway_names.each do |gateway_name|
-      its("names") { should include gateway_name }
+    dst_router_names.each do |router_name|
+      describe google_compute_router(project: dst_project_id, region: dst_region, name: router_name) do
+        it { should exist }
+      end
     end
-  end
 
-  dst_tunnel_names.each do |tunnel_name|
-    describe google_compute_vpn_tunnel(project: dst_project_id, region: dst_region, name: tunnel_name) do
+    describe google_compute_vpn_gateways(project: dst_project_id, region: dst_region) do
       it { should exist }
+      dst_gateway_names.each do |gateway_name|
+        its("names") { should include gateway_name }
+      end
+    end
+
+    dst_tunnel_names.each do |tunnel_name|
+      describe google_compute_vpn_tunnel(project: dst_project_id, region: dst_region, name: tunnel_name) do
+        it { should exist }
+      end
     end
   end
 end
